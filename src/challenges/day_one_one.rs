4c86bda9a785a9d71577632bfb2db9953380b26d
Raj Wilkhu <rajwilkhu@gmail.com>
2021-12-02 07:01:11 +0000

    Refactored to modules

---
+++ src/challenges/day_one_one.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+pub fn challenge(filename: &str)
+{
+    println!("Parsing file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+
+    let depths: Vec<i32> = contents.lines().map(|line| line.parse::<i32>().unwrap()).collect();
+
+    let mut num_increases = 0;
+    let mut prev_depth = depths[0];
+
+    depths.iter().skip(1).for_each(|depth| {
+        if *depth > prev_depth {
+            num_increases += 1;
+        }
+
+        prev_depth = *depth;
+    });
+
+    println!("Challenge 1 answer: {}", num_increases);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
-use std::fs;
+mod challenges;
 
 fn main() {
-    let filename = "input.txt";
-
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-
-    let depths: Vec<i32> = contents.lines().map(|line| line.parse::<i32>().unwrap()).collect();
-
-    let mut num_increases = 0;
-    let mut prev_depth = depths[0];
-
-    depths.iter().skip(1).for_each(|depth| {
-        if *depth > prev_depth {
-            num_increases += 1;
-        }
-
-        prev_depth = *depth;
-    });
-
-    println!("Number of increase: {}", num_increases);
+    challenges::day_one_one::challenge("input.txt");
 }
