049ff7353c07184fb6b1629ea2044e6a712b02c6
Raj Wilkhu <rajwilkhu@gmail.com>
2021-12-02 10:20:59 +0000

    Second solution of day 1

--- src/challenges/day_one_second.rs
+++ src/challenges/day_one_second.rs
@@ -8,22 +8,17 @@ pub fn challenge(filename: &str)
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    let sonar_reading: Vec<i32> = contents.lines().map(|line| line.parse::<i32>().unwrap()).collect();
+    let sonar_readings: Vec<i32> = contents.lines().map(|line| line.parse::<i32>().unwrap()).collect();
 
     let mut num_increases = 0;
     let mut prev_depth = 0;
 
-    for (pos, depth) in sonar_reading[0..(sonar_reading.len() - 3)].iter().enumerate() {
-        if pos > sonar_reading.len() - 3 {
-            break;
-        }
-
-        let slice = &sonar_reading[pos..(pos + 3)];
-        let current_depth = slice[0] + slice[1] + slice[2];
-
-        // println!("Sum {}", slice.iter().sum());
+    for (pos, depth) in sonar_readings[0..(sonar_readings.len() - 2)].iter().enumerate() {
+        let slice = &sonar_readings[pos..(pos + 3)];
+        let current_depth:i32 = slice.iter().sum();
 
         println!("Prev depth is {}", prev_depth);
+
         if pos > 0 && current_depth > prev_depth {
             num_increases = num_increases + 1;
         }
